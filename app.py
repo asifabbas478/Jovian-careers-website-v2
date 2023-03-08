f1c3ee813e5ba401655481da9138050d4e640574
asifabbas478 <30882534+asifabbas478@users.noreply.github.com>
2023-03-08 11:14:27 +0000

    dynamic pages for each job

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template,jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db1,load_jobs_from_db2
 
 app = Flask(__name__)
 
@@ -8,13 +8,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-  result_dict_list=load_jobs_from_db()
+  result_dict_list=load_jobs_from_db1()
   return render_template('home.html', jobs=result_dict_list)
 
 @app.route("/api/jobs")
 def list_jobs():
   return jsonify(JOBS)
 
+@app.route("/jobs/<id>")
+def show_jobs(id):
+  job=load_jobs_from_db2(id)
+  if not job:
+    return "Not Found", 404
+  return render_template('jobpage.html',job=job)
   
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -8,9 +8,19 @@ engine= create_engine(db_connection_string,
                         }
                       })
 
-def load_jobs_from_db():
+def load_jobs_from_db1():
   with engine.connect() as conn:
     result = conn.execute(text("select * from jobs"))
-    columns = ['id', 'job_title', 'location', 'salary', 'currency', 'job_description', 'job_qualifications']
+    columns = ['id', 'title', 'location', 'salary', 'currency', 'job_description', 'job_qualifications']
     result_dict_list = [dict(zip(columns, row)) for row in result.all()]
   return result_dict_list
+
+def load_jobs_from_db2(id):
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from jobs where id = :id"), {"id": id})
+        rows = result.all()
+        if len(rows) == 0:
+            return None 
+        else:
+            columns = ['id', 'title', 'location', 'salary', 'currency', 'job_description', 'job_qualifications']
+            return dict(zip(columns, rows[0]))

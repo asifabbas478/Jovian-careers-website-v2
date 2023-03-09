1316e937bd2064b352300392a15f6b367448187b
asifabbas478 <30882534+asifabbas478@users.noreply.github.com>
2023-03-09 05:12:52 +0000

    now all the data will get stored in database

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask,render_template,jsonify
-from database import load_jobs_from_db1,load_jobs_from_db2
+from flask import Flask,render_template,jsonify,request
+from database import load_jobs_from_db1,load_jobs_from_db2,add_application_to_db
 
 app = Flask(__name__)
 
@@ -21,6 +21,15 @@ def show_jobs(id):
   if not job:
     return "Not Found", 404
   return render_template('jobpage.html',job=job)
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data=request.form
+  job=load_jobs_from_db2(id)
+  add_application_to_db(id,data)
+  return render_template('application_submitted.html',application=data,
+                        job=job)
+  
   
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -24,3 +24,19 @@ def load_jobs_from_db2(id):
         else:
             columns = ['id', 'title', 'location', 'salary', 'currency', 'job_description', 'job_qualifications']
             return dict(zip(columns, rows[0]))
+
+
+def add_application_to_db(id, data):
+    with engine.connect() as conn:
+        query = text("insert into applications (job_id,full_name,email,linkedin_url,education,work_experience,resume_url) values(:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+        params = {
+            "job_id": id,
+            "full_name": data['full_name'],
+            "email": data['email'],
+            "linkedin_url": data['linkedin_url'],
+            "education": data['education'],
+            "work_experience": data['work'],
+            "resume_url": data['resume']
+        }
+        conn.execute(query, params)
+
